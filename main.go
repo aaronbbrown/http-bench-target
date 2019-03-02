ded7046491d5885f166d67793ab0ffc90afb20be
Aaron Brown <aaronbbrown@github.com>
2019-03-02 16:26:04 -0500

    support sleep

--- main.go
+++ main.go
@@ -2,41 +2,84 @@ package main
 
 import (
 	crand "crypto/rand"
+	"fmt"
 	"io"
 	"log"
 	"math/big"
 	"net/http"
 	"os"
 	"os/signal"
+	"runtime"
 	"strconv"
 	"syscall"
+	"time"
 
 	"github.com/urfave/negroni"
 )
 
+type CPUProfileConfig struct {
+	Iterations int
+	Sleep      time.Duration
+}
+
+func NewCPUProfileConfigFromRequest(r *http.Request) (CPUProfileConfig, error) {
+	var profile CPUProfileConfig
+	iterstr := r.URL.Query().Get("iterations")
+	if len(iterstr) < 1 {
+		iterstr = "100000"
+	}
+
+	iterations, err := strconv.Atoi(iterstr)
+	if err != nil {
+		return profile, err
+	}
+
+	sleepstr := r.URL.Query().Get("sleep")
+	if len(sleepstr) < 1 {
+		sleepstr = "0s"
+	}
+
+	sleep, err := time.ParseDuration(sleepstr)
+	if err != nil {
+		return profile, err
+	}
+
+	profile.Iterations = iterations
+	profile.Sleep = sleep
+
+	return profile, nil
+}
+
+func thread_cpu_usage() time.Duration {
+	r := syscall.Rusage{}
+	syscall.Getrusage(syscall.RUSAGE_SELF, &r)
+	return time.Duration(r.Stime.Nano() + r.Utime.Nano())
+}
+
 func main() {
+	fmt.Printf("GOMAXPROCS: %d\n", runtime.GOMAXPROCS(0))
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/cpu", func(w http.ResponseWriter, r *http.Request) {
-		iterstr := r.URL.Query().Get("iterations")
-		if len(iterstr) < 1 {
-			iterstr = "100000"
-		}
+		cpustart := thread_cpu_usage()
 
-		iterations, err := strconv.Atoi(iterstr)
+		profile, err := NewCPUProfileConfigFromRequest(r)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			return
 		}
 
-		for i := 1; i < iterations; i++ {
+		for i := 1; i < profile.Iterations; i++ {
 			_, err := crand.Int(crand.Reader, big.NewInt(27))
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
 		}
-		io.WriteString(w, "Generated "+strconv.Itoa(iterations)+" random strings")
+
+		time.Sleep(profile.Sleep)
+		cpuend := thread_cpu_usage()
+		fmt.Fprintf(w, "iterations=%d cputime=%d\n", profile.Iterations, cpuend.Nanoseconds()-cpustart.Nanoseconds())
+
 	})
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
